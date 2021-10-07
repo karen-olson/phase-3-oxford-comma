0405b9f174398677aed2fe0aa20db1243674aa75
Karen Olson <karmarie@gmail.com>
2021-10-07 18:58:21 -0500

    commit tree 59304469602add4946d5d54372f8e45d89e1907d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    case array.length
+    when 1
+        array.join
+    when 2
+        array.join(" and ")
+    else
+        last_element = array.pop
+        first_elements = array.join(", ") + ", and "
+        first_elements + last_element
+    end
 end
+
+# solution
+
+# def oxford_comma(array)
+#   return array.join(" and ") if array.size <3
+#   
+#   array[-1] = "and #{array[-1]}"
+#   array.join(", ")
+# end
